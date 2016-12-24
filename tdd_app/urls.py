e458567fe3d7763faf2bf8ef09b29f91f94ebc6b
Sergey <dagerok22@gmail.com>
2016-12-24 11:07:08 +0700

    First unit test and url mapping, dummy view

--- tdd_app/urls.py
+++ tdd_app/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import tdd_app.urls
+
+from tdd_app.views import home_page
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(tdd_app.urls)),
+    url(r'^$', home_page, name='home_page'),
 ]
