3a7746f7b64e4da98f4364d5596c0c4f778470ca
Sergey <dagerok22@gmail.com>
2016-12-26 19:17:21 +0700

    Made side function for rows text comparing

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,10 @@
 import unittest
 
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+
 
 
 # def get_firefoxbinary():
@@ -28,7 +29,9 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # The page updates again, and now shows both items on her list
 # browser.quit()
 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         binary = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox')
         self.browser = webdriver.Firefox(firefox_binary=binary)
@@ -36,6 +39,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -58,23 +66,20 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(any('Buy peacock feathers' == row.text for row in rows),
-        #                 msg='New to-do item does not appear in the table -- it\'s text was:\n%s' % (table.text, ))
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
-        browser.quit()
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
